c6a101f04eae797bea0b98accd6827e232825554
Kyle Burton <kyleburton@Kyles-MacBook-Pro.local>
2018-08-25 19:12:58 -0700

    deleting extra things

--- KCI/data/scripts/loadColors.js
+++
@@ -1,9 +0,0 @@
-//loadColors("white-White,blue-Blue,colorlessLand-Colorless Land,gold-Gold")
-function loadColors(colors) {
-	var endResult = ""
-	var colorList = colors.split(",")
-	for (i = 0; i < colorList.length; i++) {
-		endResult += "<option value='" + colorList[i].split("-")[0] + "'>" + colorList[i].split("-")[1] + "</option>"
-	}
-	document.getElementById("colorSelection").innerHTML = endResult
-}
--- KCI/data/scripts/loadScript.js
+++
@@ -1,8 +0,0 @@
-function loadScript(scriptName){
-	var script = document.createElement("script")
-	script.setAttribute("type","text/javascript")
-	script.setAttribute("src", scriptName)
-	if (typeof script != "undefined") {
-		document.getElementsByTagName("head")[0].appendChild(script)
-	}
-}
