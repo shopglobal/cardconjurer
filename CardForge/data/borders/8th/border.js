bc614b610c1c1f5eccea5b4b6d6d4736f52d37be
Kyle Burton <kyleburton@Kyles-MacBook-Pro.local>
2018-08-25 17:58:05 -0700

    renames

---
+++ CardForge/data/borders/8th/border.js
@@ -0,0 +1,48 @@
+var titleFont = "44px matrixb"
+var titleFontSpacing = "0.5px"
+var titleX = 66
+var titleY = 62
+
+var manaCostRadius = 16.5
+var manaCostX = 652
+var manaCostY = 68
+
+var typeFont = "37.5px matrixb"
+var typeFontSpacing = "0px"
+var typeX = 74
+var typeY = 598
+
+var textFont = "px mplantin"
+var textFontSpacing = 1.2
+var textX = 76
+var textY = 656
+var textWidth = 680
+
+var ptFont = "bold 48px matrix"
+var ptFontSpacing = "0.3px"
+var ptTextX = 626
+var ptTextY = 936
+var ptX = 541
+var ptY = 920.5
+var ptWidth = 162
+var ptHeight = 84
+
+var eighthInfo = true
+var eighthInfoY = 956
+var m15Info = false
+var m15InfoY = 992
+
+var setSymbolY = 615
+var setSymbolRight = 689
+var setSymbolWidth = 77
+var setSymbolHeight = 44
+
+var watermarkWidth = 520
+var watermarkHeight = 208
+var watermarkY = 796
+
+var rareStampY = 958
+
+loadColors("white-White,whiteLand-White Land,blue-Blue,blueLand-Blue Land,black-Black,blackLand-Black Land,red-Red,redLand-Red Land,green-Green,greenLand-Green Land,gold-Gold,goldLand-Gold Land,colorless-Colorless,colorlessLand-Colorless Land,artifact-Artifact,oldArtifact-Old Artifact,oldGold-Old Gold,oldGoldLand-Old Gold Land")
+document.getElementById("secondColorSelection").innerHTML = document.getElementById("colorSelection").innerHTML
+document.getElementById("thirdColorSelection").innerHTML = document.getElementById("colorSelection").innerHTML
